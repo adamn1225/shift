9c6bcbff015f55e5af9df3bd129fc1f01583c809
adamn1225 <anoah1225@gmail.com>
2025-09-10 18:36:37 -0400

    Update version to 1.0.6 in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read_readme():
 
 setup(
     name="shift-cli",
-    version="1.0.5",
+    version="1.0.6",
     description="Universal document and PDF toolkit - Convert, compress, edit, and process documents",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
